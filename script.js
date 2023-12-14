a6c6a11ee971a0bb4bb0b55f7bab51651c17c5dd
Austin Smith <53031473+asmith94@users.noreply.github.com>
2023-12-13 19:10:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeImage (element) {
+    element.src = "./images/cafe-neko.png";
+}
+function changeBack(element) {
+    element.src = "./images/stonepunk.png";
+}
+function updateCart(element) {
+    element.innerText++;
+}
